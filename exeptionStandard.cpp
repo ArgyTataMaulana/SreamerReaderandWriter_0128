095c08fbfea002ee5cf6b0660101aef007f55a07
ArgyTataMaulana <halmaheru37@gmail.com>
2025-06-10 18:41:38 +0700

    Menambahkan blok try-catch untuk menangani akses array di luar batas

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -7,4 +7,16 @@ using namespace std;
 
 int main() {
     cout << "Awal Program" << endl; // penanda i....
+    try
+    {
+        array<int, 3> data = {10, 20, 30 };
+        // pesan array integer 3 element
+        cout << data.at(5)<< endl;
+    }
+    catch(exception& e)
+    {
+        // penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan dieksekusi karena array data hanya memiliki tiga elemen*/
+    }
 }
