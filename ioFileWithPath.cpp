85c38859e94c60daf8c009f19bfb05ff5899b8aa
ArgyTataMaulana <halmaheru37@gmail.com>
2025-06-10 18:31:38 +0700

    membuat kondisi dimana jika file ada maka melakukkan perulangan setiapbaris, jika tidak ada maka menampilkan unable to open file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -33,4 +33,20 @@ int main() {
     // menunjuk ke sebuah file
     infile.open(NamaFile, ios::in);
     cout << endl << ">- Membuka dan membaca file " << endl;
+    // jika file ada maka 
+    if(infile.is_open())
+    {
+        // Melakukkan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            /* code */
+            // dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        // tutup file tersebut setelah selesai
+        infile.close();        
+    }
+    // jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
 }
